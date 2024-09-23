746a3ce60d5670707b01872f8c79bf9cea71bb4a
ShivaNKA0 <shiva.kanneboina@gmail.com>
2024-09-23 12:53:46 -0400

    nav bar additions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/Uso6XTuymfAdIKNUEID0z7SWDMOd4JKLoGw2cexPCuUVlaTB"
+];
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in the making)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url}/>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
