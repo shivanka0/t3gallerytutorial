96ca1707eef164686934f3c330885e7911cca0f2
ShivaNKA0 <shiva.kanneboina@gmail.com>
2024-09-23 14:47:45 -0400

    db to ui connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,18 @@ import { db } from "~/server/db";
 
 export const dynamic = 'force-dynamic';
 
-const mockUrls = [
-  "https://utfs.io/f/Uso6XTuymfAdIKNUEID0z7SWDMOd4JKLoGw2cexPCuUVlaTB",
-  "https://utfs.io/f/Uso6XTuymfAd9fcgHCmEcaeQhuAf0v8UjXz32mtNn6J4GH5d",
-];
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
+
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { asc }) => [asc(model.id)],
+  });
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (<div key={post.id}>{post.name}</div>))}
-        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
+        {[...images, ...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48 flex flex-col">
             <img src={image.url}/>
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
