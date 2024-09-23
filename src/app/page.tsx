fefb5393ef3c905d93e6d1efd65e030db83b2318
ShivaNKA0 <shiva.kanneboina@gmail.com>
2024-09-23 15:49:02 -0400

    images unique to each user with auth to get their unique images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import Link from "next/link";
 import { db } from "~/server/db";
-
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 export const dynamic = 'force-dynamic';
 
-
-export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { asc }) => [asc(model.id)],
-  });
-  return (
-    <main className="">
+async function Images() {
+    const images = await db.query.images.findMany({
+        orderBy: (model, { asc }) => [asc(model.id)],
+      });
+    return (
       <div className="flex flex-wrap gap-4">
         {[...images, ...images, ...images, ...images].map((image, index) => (
           <div key={image.id + "-" + index} className="w-48 flex flex-col">
@@ -18,6 +16,18 @@ export default async function HomePage() {
           </div>
         ))}
       </div>
+    )
+}
+
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please sigin in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
